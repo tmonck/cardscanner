a03c10784b2aa98f02f857c42ee6f091a682e092
Tom Monck <junja430@gmail.com>
2021-09-07 19:02:59 -0400

    Move things around and add the google packages.

---
+++ TestTcgScanner/TestTcgScanner/MtgCardScanningPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+
+namespace TestTcgScanner
+{
+    public partial class MtgCardScanningPage : ContentPage
+    {
+        public MtgCardScanningPage()
+        {
+            InitializeComponent();
+            MediaPicker.CapturePhotoAsync();
+        }
+
+        private string CardName;
+        private string CardNumber;
+        private string CardSet;
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            CounterLabel.Text = $"Current count: 0";
+        }
+    }
+}
