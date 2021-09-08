18e8f71366e9f5e922730a3bb4107321f118d5a1
Tom Monck <junja430@gmail.com>
2021-09-07 20:17:40 -0400

    Get App loading in debug mode

--- TestTcgScanner/TestTcgScanner/MainPage.xaml.cs
+++ TestTcgScanner/TestTcgScanner/MainPage.xaml.cs
@@ -1,20 +1,22 @@
 ﻿using System;
+using Microsoft.Maui;
 using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.PlatformConfiguration.WindowsSpecific;
 
 namespace TestTcgScanner
 {
-	public partial class MainPage : ContentPage
-	{
-		public MainPage()
-		{
-			InitializeComponent();
-		}
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
 
-		int count = 0;
-		private void OnCounterClicked(object sender, EventArgs e)
-		{
-			count++;
-			CounterLabel.Text = $"Current count: {count}";
-		}
-	}
+        int count = 0;
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+            CounterLabel.Text = $"Current count: {count}";
+        }
+    }
 }
